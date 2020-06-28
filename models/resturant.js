4e91bf0f1ef1a4c43cd54d827f9e5abd21e3c522
arsalannasir <anasir@nisum.com>
2020-06-28 19:23:27 +0500

    Server Established

---
+++ models/resturant.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const resturantScheme = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("resturants", resturantScheme);
