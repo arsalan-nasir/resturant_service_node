eda70a37da439c6cb1fc40353ea274e0a130c644
arsalannasir <anasir@nisum.com>
2020-07-06 18:42:48 +0500

    Database Connected

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const options = {
   useFindAndModify: false,
   useUnifiedTopology: true,
 };
+
 mongoose.connect(process.env.MONGODB_URI || config.mongo.uri, options).then(
   () => {
     console.log("Database connection established!");
