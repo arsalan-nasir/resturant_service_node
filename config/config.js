22a74e9c3cb96d2d019ef7f3676e24f8d656ac60
arsalannasir <anasir@nisum.com>
2020-07-06 18:55:11 +0500

    config added

--- config/config.js
+++ config/config.js
@@ -6,9 +6,8 @@ dotenv.config();
 var all = {
   env: process.env.NODE_ENV,
   port: process.env.PORT || 3000,
-  ip: process.env.IP || "127.0.0.1",
   mongo: {
-    uri: process.env.MONGOLAB_URI || "mongodb://localhost/resturants",
+    uri: process.env.MONGOLAB_URI,
   },
 };
 module.exports = all;
